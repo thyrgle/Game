68301819b94b94b4208be096e25d39417da91e55
Christopher Sumnicht <csumnicht@berkeley.edu>
2016-10-19 14:28:22 -0700

    Very basic skeleton code for Quarto and Tant Fant.

---
+++ tant_fant.py
@@ -0,0 +1,15 @@
+def initial_pos():
+    # Maybe change to just a variable instead of function.
+    pass
+
+
+def do_move(pos, move):
+    pass
+
+
+def gen_moves(pos):
+    pass
+
+
+def primitive(pos):
+    pass
