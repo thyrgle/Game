9594a10b8768f836785f21b5507b9babf48c1b06
Andrea Liu <andrealiu@berkeley.edu>
2016-11-07 14:18:28 -0800

    added docstrings

--- quarto.py
+++ quarto.py
@@ -1,3 +1,4 @@
+# Enums for quarto.
 DRAW = "D"
 WIN = "W"
 UNDECIDED = "U"
@@ -5,11 +6,18 @@ LOSE = "L"
 TIE = "T"
 
 class Piece():
+    """ 
+    This is the piece class.
+    """
     attributes = None
     full_name = ""
     abbreviation = ""
 
     def __init__(self, attributes):
+        """ 
+        Constructor for our piece class with support for 
+        the various pieces in the game of Quarto.
+        """
         self.attributes = attributes
         if attributes & 0b0001:
             self.full_name += "Tall"
@@ -36,23 +44,39 @@ class Piece():
             self.full_name += " hollow-top"
             self.abbreviation += "H"
 
-    # returns a list of attributes
     def get_attributes(self):
+        """ 
+        Returns a list of attributes.
+        """
         return self.attributes
 
     def get_piece_name(self):
+        """
+        Returns the name of the piece.
+        """
         return self.full_name
 
     def get_piece_abbr(self):
+        """
+        Returns the abbreviation of the piece.
+        """
         return self.abbreviation
 
 class Board():
+    """
+    This is the board class.
+    """
     pieces = []
     cols_count = 4
     rows_count = 4
     board = None
 
     def __init__(self):
+        """
+        Constructor for our board class. 
+        Appends all the pieces of quarto to our array of pieces.
+        Implemented by a 4x4 2D array.
+        """
         self.board = [[None for x in range(self.cols_count)] for y in
                       range(self.rows_count)]
         #short: 0, tall: 1
@@ -77,25 +101,43 @@ class Board():
         self.pieces.append(Piece(0b1111))
 
     def get_board(self):
+        """
+        Returns the board. 
+        """
         return self.board
 
     def get_pieces(self):
+        """
+        Returns the pieces array.
+        """
         return self.pieces
 
     def get_pieces_names(self):
+        """
+        Returns the names of all the pieces in our pieces array.
+        """
         return ["(" + str(i) + "): " + self.pieces[i].get_piece_name()
                 for i in range(0, len(self.pieces))]
 
     def get_rows(self):
+        """
+        Returns the rows of the board.
+        """
         return self.board
 
     def get_cols(self):
+        """
+        Returns the columns of the board.
+        """
         cols = []
         for row in self.board:
             cols.append([row[i] for i in range(self.rows_count)])
         return cols
 
-    def shared_attributes(self, lst): #finds list of shared attributes among pieces
+    def shared_attributes(self, lst):
+        """
+        Returns a list of shared attributes among pieces.
+        """
         attributes_list = [piece.get_attributes() for piece in lst
                            if piece] #nested list of all attributes
         if len(attributes_list) != self.rows_count:
@@ -108,12 +150,18 @@ class Board():
         return win or win2
 
     def check_win_horizontal(self):
+        """
+        Returns a boolean indicating whether or not a horizontal win has occurred.
+        """
         for row in self.get_rows(): #0 to 3
             if self.shared_attributes(row): #if there are shared attributes
                 return True
         return False
 
     def check_win_vertical(self):
+        """
+        Returns a boolean indicating whether or not a vertical win has occurred.
+        """
         attr_list = [[] for i in range(self.rows_count)]
         for row in self.get_rows():
             for i in range(len(row)):
@@ -124,6 +172,9 @@ class Board():
         return False
 
     def check_win_diagonal(self):
+        """
+        Returns a boolean indicating whether or not a diagonal win has occurred.
+        """
         ltr_diag = []
         rtl_diag = []
         i, j = 0, 3
@@ -138,18 +189,27 @@ class Board():
         return False
 
     def player(self):
+        """
+        Returns whose turn it is.
+        """
         if len(self.pieces)%2 == 0:
             return 1
         else:
             return 2
 
     def other_player(self):
+        """ 
+        Returns the other person's turn.
+        """
         if len(self.pieces)%2 == 0:
             return 2
         else:
             return 1
 
     def print_board(self):
+        """
+        Prints the board.
+        """
         for row in self.board:
             pr = []
             for piece in row:
@@ -157,9 +217,12 @@ class Board():
                     pr.append(piece.get_piece_abbr())
                 else:
                     pr.append(None)
-            print pr
+            print(pr)
 
     def place_piece(self, piece, row, col):
+        """
+        Places the piece in the specified location.
+        """
         if not self.board[row][col] and piece in self.pieces:
             self.board[row][col] = piece
             # del self.pieces[piece]
@@ -171,11 +234,16 @@ class Board():
 quarto_board = None
 
 def initial_position():
-    # 2d array: 4x4 initialized to
+    """
+    Initializes the board, which is a 4x4 2D array.
+    """
     return Board()
 
-# takes in a 4x4 array - state is of class Board
 def primitive(state):
+    """
+    Returns whether or not the current game is a TIE, WIN, or UNDECIDED.
+    This function takes in a state, which is an instance of Board.
+    """
     if (state.check_win_horizontal() or state.check_win_vertical() or
             state.check_win_diagonal()):
         return WIN
@@ -185,31 +253,42 @@ def primitive(state):
     return UNDECIDED
 
 def gen_moves(state):
-  # possible pieces for your opponent to give you
+    """
+    Generates a list of possible pieces for your opponent to give you.
+    """
     return state.get_pieces_names()
 
 def do_move(move, state):
-  #unpack
-  # move = (piece, row, col)
+    """
+    Makes the move.
+    """
     state.place_piece(move[0], move[1], move[2])
     return state
 
 def solve(state):
+    """
+    Solver for our game instance.
+    """
     return primitive(state)
 
 def main():
-    print "Starting game of Quarto..."
+    """
+    The main function that keeps the game running.
+    This is essentially an interface between our quarto game
+    and the player.
+    """
+    print("Starting game of Quarto...")
     print()
     board = initial_position()
     while True:
-        print "------------"
+        print("------------")
         print("Player", board.player(), "'s turn: ")
-        print "------------"
-        print "Current state of board: "
+        print("------------")
+        print("Current state of board: ")
         board.print_board()
         print()
-        print "Available pieces: "
-        print gen_moves(board)
+        print("Available pieces: ")
+        print(gen_moves(board))
         print()
         piece_index = input("Player " +str(board.other_player()) +
                             " please pick a piece to give to Player "
